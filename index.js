6e7bd0d19cb7e6a1c19aad67202d662488c18cb7
Jilli Hadi Tian <jilli.ht@gmail.com>
2024-10-20 08:37:47 +0700

    sampai create new pages

--- index.js
+++ index.js
@@ -12,4 +12,15 @@ ROUTE("GET /dashboard", function () {
   this.view("pages/dashboard");
 });
 
+ROUTE("GET /buku", function () {
+  this.view("pages/books");
+});
+
+ROUTE("GET /penerbit", function () {
+  this.view("pages/publisher");
+});
+
+ROUTE("GET /pengguna", function () {
+  this.view("pages/users");
+});
 HTTP("debug", { port: 5000 });
